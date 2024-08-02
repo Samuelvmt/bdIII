8577e64bb4604c75663c261ad4ab5152d62a451a
Samuelvmt <145499760+Samuelvmt@users.noreply.github.com>
2024-08-02 20:14:19 -0300

    Add files via upload
    fazendo ligação das tabelas

--- scriptsql/dbPets.sql
+++ scriptsql/dbPets.sql
@@ -7,20 +7,24 @@ use dbpets;
 -- visualizando banco de dados
 show databases;
 -- tabelas no banco de dados
+create table tbfuncionarios(
+cod_func int not null auto_increment,
+nome varchar(100) not null,
+email varchar(100),
+telcel char(10),
+cpf char(14),
+primary key(cod_func)
+);
 create table tbusuarios(
 cod_usu int not null auto_increment,
 nome varchar(50) not null,
 senha varchar(10) not null,
-primary key(cod_usu)
-);
-
-create table tbfuncionarios(
-cod_func int not null auto_increment,
-nome varchar(100) not null,
-email varchar(100)
-telcel char(10),
+cod_func int not null,
+primary key(cod_usu),
+foreign key(cod_func)references tbfuncionarios(cod_func)
 );
 -- visualizando tabelas
 show tables;
 -- visualizando a estrutura das tabelas
 desc tbusuarios;
+desc tbfuncionarios;
