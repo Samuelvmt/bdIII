0b2b5e90128f6c3ad12abc86402783e81721f136
Samuelvmt <145499760+Samuelvmt@users.noreply.github.com>
2024-08-02 19:27:52 -0300

    Add files via upload

---
+++ scriptsql/dbPets.sql
@@ -0,0 +1,9 @@
+-- apagar o banco de dados
+drop database dbpets;
+-- criar banco de dados
+create database dbpets;
+-- acessar o banco de dados
+use dbpets;
+-- visualizando banco de dados
+show databases;
+-- 
